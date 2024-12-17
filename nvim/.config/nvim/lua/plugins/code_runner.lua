4446a005515da363ced20fdb4993d5f76f41b692
rushin236 <rushikeshshindesandesh@gmail.com>
2024-12-17 21:31:27 +0530

    changed config of code runner to add args while running c file

--- nvim/.config/nvim/lua/plugins/code_runner.lua
+++ nvim/.config/nvim/lua/plugins/code_runner.lua
@@ -1,5 +1,44 @@
 return {
   "CRAG666/code_runner.nvim",
   cmd = { "RunCode", "RunFile", "RunProject" },
-  config = true,
+  config = function()
+    require("code_runner").setup({
+      filetype = {
+        javascript = "node",
+        java = {
+          "cd $dir &&",
+          "javac $fileName &&",
+          "java $fileNameWithoutExt",
+        },
+        c = function(...)
+          c_base = {
+            "cd $dir &&",
+            "gcc $fileName -o",
+            "$fileNameWithoutExt",
+          }
+          local c_exec = {
+            "&& $dir/$fileNameWithoutExt",
+          }
+          vim.ui.input({ prompt = "Add more args:" }, function(input)
+            c_base[4] = input
+            require("code_runner.commands").run_from_fn(vim.list_extend(c_base, c_exec))
+          end)
+        end,
+        cpp = {
+          "cd $dir &&",
+          "g++ $fileName",
+          "-o $fileNameWithoutExt &&",
+          "$dir/$fileNameWithoutExt",
+        },
+        python = "python -u",
+        sh = "bash",
+        ruby = "ruby",
+        rust = {
+          "cd $dir &&",
+          "rustc $fileName &&",
+          "$dir/$fileNameWithoutExt",
+        },
+      },
+    })
+  end,
 }
