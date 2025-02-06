dc496b97e08bb3379fd57e7570f92f05f4d58cf0
rushin236 <rushikeshshindesandesh@gmail.com>
2025-02-07 01:23:49 +0530

    updated config to the latest changes

--- nvim/.config/nvim/lua/plugins/code_runner.lua
+++ nvim/.config/nvim/lua/plugins/code_runner.lua
@@ -14,9 +14,19 @@ return {
           local c_exec = {
             "&& $dir/$fileNameWithoutExt",
           }
-          vim.ui.input({ prompt = "Add more args:" }, function(input)
-            c_base[4] = input
-            require("code_runner.commands").run_from_fn(vim.list_extend(c_base, c_exec))
+
+          vim.ui.input({ prompt = "Add compilation args (e.g., -Wall -g): " }, function(compile_args)
+            if compile_args and compile_args ~= "" then
+              table.insert(c_base, 4, compile_args) -- Append compilation arguments
+            end
+
+            vim.ui.input({ prompt = "Add execution args: " }, function(exec_args)
+              if exec_args and exec_args ~= "" then
+                table.insert(c_exec, exec_args) -- Append execution arguments
+              end
+
+              require("code_runner.commands").run_from_fn(vim.list_extend(c_base, c_exec))
+            end)
           end)
         end,
       },
