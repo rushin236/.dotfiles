c7b20e219e743316d84c7c96d205296807e2575b
rushin236 <rushikeshshindesandesh@gmail.com>
2025-02-25 01:30:31 +0530

    added polybar changed the rofi theme and added polybar to i3

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Kill all running Polybar instances
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload toph &
+  done
+else
+  polybar --reload toph &
+fi
