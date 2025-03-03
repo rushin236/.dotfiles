c5777859c4322334a7fd1826386d27afd70b8a33
rushin236 <rushikeshshindesandesh@gmail.com>
2025-03-03 21:14:47 +0530

    changed the i3lock folder in powermenu.sh

--- rofi/.config/rofi/powermenu/powermenu.sh
+++ rofi/.config/rofi/powermenu/powermenu.sh
@@ -2,7 +2,7 @@
 options="Lock\nLogout\nReboot\nShutdown"
 selected=$(echo -e "$options" | rofi -dmenu -p "Power Menu" -theme ~/.config/rofi/config.rasi)
 case $selected in
-    "Lock") ~/.config/i3/scripts/i3lock.sh ;;
+    "Lock") ~/.config/i3/lock/i3lock.sh ;;
     "Logout") i3-msg exit ;;
     "Reboot") systemctl reboot ;;
     "Shutdown") systemctl poweroff ;;
