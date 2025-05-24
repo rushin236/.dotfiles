de1b3b8c4baa7632c545763714f6b54c4cb76dbf
rushin236 <rushikeshshindesandesh@gmail.com>
2025-05-25 03:21:56 +0530

    added custom command to run cpp files

--- nvim/.config/nvim/lua/plugins/code_runner.lua
+++ nvim/.config/nvim/lua/plugins/code_runner.lua
@@ -31,6 +31,32 @@ return {
             end)
           end)
         end,
+        cpp = function()
+          local cpp_base = {
+            "cd $dir &&",
+            "gcc $fileName -o",
+            "$dir/$fileNameWithoutExt",
+          }
+          local cpp_exec = {
+            "&& $dir/$fileNameWithoutExt",
+          }
+
+          vim.ui.input({ prompt = "Add compilation args (e.g., -Wall -g): " }, function(compile_args)
+            if compile_args and compile_args ~= "" then
+              table.insert(cpp_base, 4, compile_args) -- Append compilation arguments
+            end
+
+            vim.ui.input({ prompt = "Add execution args: " }, function(exec_args)
+              if exec_args and exec_args ~= "" then
+                table.insert(cpp_exec, exec_args) -- Append execution arguments
+              end
+
+              vim.list_extend(cpp_base, cpp_exec)
+              local cmd_str = table.concat(cpp_base, " ")
+              require("code_runner.commands").run_from_fn(cmd_str)
+            end)
+          end)
+        end,
       },
     })
 
